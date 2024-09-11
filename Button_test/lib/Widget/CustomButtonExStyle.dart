823b7f15b1a833aaf3a04e2ce186b488ca2a48b6
daesung <daesung@mediaweb.co.kr>
2024-09-11 14:16:16 +0900

    DaeseongFlutter

---
+++ Button_test/lib/Widget/CustomButtonExStyle.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomButtonExStyle {
+  static final ButtonStyle style1 = ElevatedButton.styleFrom(backgroundColor: Colors.white70);
+  static final ButtonStyle style2 = ElevatedButton.styleFrom(backgroundColor: Colors.white38);
+  static final ButtonStyle style3 = ElevatedButton.styleFrom(backgroundColor: Colors.white10);
+  static final ButtonStyle style4 = ElevatedButton.styleFrom(backgroundColor: Colors.white24);
+  static final ButtonStyle style5 = ElevatedButton.styleFrom(backgroundColor: Colors.white30);
+}
---
+++ toast_test/android/app/src/main/kotlin/com/example/toast_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.toast_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
